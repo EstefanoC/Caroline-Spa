98fbe9ac974ee1679dbc642eb1c34157efdfa30b
EstefanoC <estefano.jesus6@gmail.com>
2021-06-11 21:23:00 -0400

    mend

--- include/conexion.php
+++ include/conexion.php
@@ -4,7 +4,6 @@
 
 try {
     // $db = new mysqli('localhost', 'root', '', 'carolina');
-    // $db = new mysqli('fdb27.runhosting.com', '3827358_carolinaspa', 'carolinaspa123.', '3827358_carolinaspa', 3306);
     $db = mysqli_connect('sql10.freesqldatabase.com', 'sql10418608', 'DlcWI2qJhQ', 'sql10418608', 3306);
     $acentos = $db->query("SET NAMES 'utf8'");
 } catch (Exception $e) {
