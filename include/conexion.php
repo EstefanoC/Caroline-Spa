27b7c2f43e6bb75744ad95f80787a8308356fc71
EstefanoC <estefano.jesus6@gmail.com>
2021-07-11 00:18:19 -0400

    update sql in freemysqlhosting v1.0.0

--- include/conexion.php
+++ include/conexion.php
@@ -4,7 +4,7 @@
 
 try {
     // $db = new mysqli('localhost', 'root', '', 'carolina');
-    $db = mysqli_connect('sql10.freesqldatabase.com', 'sql10418608', 'DlcWI2qJhQ', 'sql10418608', 3306);
+    $db = mysqli_connect('sql10.freemysqlhosting.net', 'sql10424238', 'JGSwQ8hgAs', 'sql10424238');
     $acentos = $db->query("SET NAMES 'utf8'");
 } catch (Exception $e) {
     echo $e->getMessage();
